565b08d55b54c134fc428dfe3b6b46863a7d67b2
JustATangMan <jasontang127@gmail.com>
2023-07-04 18:15:10 -0400

    works on non-EXIF photos, puts photos in diff folder

--- ImageDateStamper.py
+++ ImageDateStamper.py
@@ -1,15 +1,17 @@
 from PIL import Image, ExifTags, ImageDraw, ImageFont, ImageOps
 from PIL.ExifTags import TAGS
-import glob, os
+import glob
+import os
+import time
+import gc
 
 # 256, width
 # 257, length
 # 274, orientation
 # 306, date
 
-font = ImageFont.truetype("Arial.ttf", 50)
 os.chdir("./Photos")
-
+os.mkdir("./Dated")
 
 def print_stats(image):
     exif = image.getexif()
@@ -17,19 +19,36 @@ def print_stats(image):
     length = exif.get(257)
     date = exif.get(306)
     orient = exif.get(274)
-    print("width: " + str(width) + " length: " + str(length) + " date: " + str(date) + " orient: " + str(orient))
+    # print("width: " + str(width) + " length: " + str(length) + " date: " + str(date) + " orient: " + str(orient))
     return width, length, orient, date
 
 
 for file in glob.glob("*.jpg"):
     filename, extension = os.path.splitext(file)
-    print(filename)
     im = Image.open(filename + extension)
     im = ImageOps.exif_transpose(im)
     im_width, im_length, im_orient, im_date = print_stats(im)
+    if im_width is None: # no exif data use file data
+        im_width = im.width
+        im_length = im.height
+        im_date = time.ctime(os.path.getctime(filename + extension))
+    elif im_orient is None: # exif oriented, swap length and width
+        hold = im_width
+        im_width = im_length
+        im_length = hold
     draw = ImageDraw.Draw(im)
-    if im_orient is None:
-        draw.text((im_length*0.80, im_width*0.95), str(im_date), None, font)
-    else:
-        draw.text((im_width * 0.85, im_length * 0.95), str(im_date), None, font)
-    im.save("dated_" + filename + extension, "JPEG")
+    os.chdir("../")
+    size = (im_width // 60)
+    if size < 10:
+        size = 10
+    if size > 100:
+        size = 100
+    font = ImageFont.truetype("Arial.ttf", size)
+    os.chdir("./Photos")
+    draw.text((im_width*0.8, im_length*0.95), str(im_date), None, font)
+    im = im.convert('RGB')
+    os.chdir("./Dated")
+    im.save(filename + extension, "JPEG")
+    os.chdir("../")
+    # del font
+    # gc.collect()
