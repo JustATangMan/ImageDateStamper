bb949eef7423b803c3dc4d6e8286a691cd474059
JustATangMan <jasontang127@gmail.com>
2023-07-05 13:57:57 -0400

    stamping color change

--- ImageDateStamper.py
+++ ImageDateStamper.py
@@ -13,11 +13,14 @@ import gc
 os.chdir("./Photos")
 os.mkdir("./Dated")
 
+
 def print_stats(image):
     exif = image.getexif()
     width = exif.get(256)
     length = exif.get(257)
     date = exif.get(306)
+    if date is not None:
+        date = date.split()[0]
     orient = exif.get(274)
     # print("width: " + str(width) + " length: " + str(length) + " date: " + str(date) + " orient: " + str(orient))
     return width, length, orient, date
@@ -28,24 +31,27 @@ for file in glob.glob("*.jpg"):
     im = Image.open(filename + extension)
     im = ImageOps.exif_transpose(im)
     im_width, im_length, im_orient, im_date = print_stats(im)
-    if im_width is None: # no exif data use file data
+    if im_width is None:  # no exif data use file data
         im_width = im.width
         im_length = im.height
-        im_date = time.ctime(os.path.getctime(filename + extension))
-    elif im_orient is None: # exif oriented, swap length and width
+        im_date = time.ctime(os.path.getctime(filename + extension)).split()  # parse file date, exclude day of week and time
+        im_date.pop(3)
+        im_date.pop(0)
+        im_date = " ".join([str(x) for x in im_date])
+    elif im_orient is None:  # exif oriented, swap length and width
         hold = im_width
         im_width = im_length
         im_length = hold
     draw = ImageDraw.Draw(im)
     os.chdir("../")
-    size = (im_width // 60)
+    size = ((im_width + im_length) // 50)
     if size < 10:
         size = 10
     if size > 100:
         size = 100
     font = ImageFont.truetype("Arial.ttf", size)
     os.chdir("./Photos")
-    draw.text((im_width*0.8, im_length*0.95), str(im_date), None, font)
+    draw.text((im_width * 0.8, im_length * 0.95), str(im_date), (255,0,0), font)
     im = im.convert('RGB')
     os.chdir("./Dated")
     im.save(filename + extension, "JPEG")
